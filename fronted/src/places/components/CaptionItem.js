a0c11e003b96aef1633395fdec3085572df3c4c1
Aryan <cse200001010@iiti.ac.in>
2023-06-30 20:36:08 +0530

    Captions styled and copy functionality added

--- fronted/src/places/components/CaptionItem.js
+++ fronted/src/places/components/CaptionItem.js
@@ -1,14 +1,38 @@
-import React from 'react'
-import { FiClipboard } from "react-icons/fi";
+import React, { useState } from "react";
+import { BsClipboard, BsFillClipboardFill } from "react-icons/bs";
 
 export const CaptionItem = (props) => {
+  const [touch, setTouch] = useState(false);
+
+  const showFilled = async () => {
+    try{
+      await navigator.clipboard.writeText(props.caption);
+      setTouch(true);
+      setTimeout(() => {
+        setTouch(false);
+      }, 2500);
+        }
+    catch{
+      console.error('Failed to copy text');
+    }
+  }
+
   return (
     <li>
-        <div>
-            <p>{props.caption}</p>
-            <FiClipboard/>
+      <div className="caption-item__container">
+        <p className="caption-item__text">{props.caption}</p>
+        <div className="caption-item__icon__container">
+          {touch ? (
+            <BsFillClipboardFill className="caption-item__icon" size={21} />
+          ) : (
+            <BsClipboard
+              className="caption-item__icon"
+              size={21}
+              onClick={showFilled}
+            />
+          )}
         </div>
-        
+      </div>
     </li>
-  )
-}
+  );
+};
--- fronted/src/places/components/CaptionList.js
+++ fronted/src/places/components/CaptionList.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { CaptionItem } from './CaptionItem'
 
+import "./CaptionList.css"
+
 export const CaptionList = (props) => {
   return (
-    <ul>
+    <ul className='caption-lists'>
     {props.captions.map(caption => {
         return <CaptionItem caption={caption}/>
 
